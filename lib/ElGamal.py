8d14f96c8aa1bcf4b7e41f3e9816063284c73f83
Stefan Brankovic <79327006+stefcon@users.noreply.github.com>
2023-06-04 01:24:04 +0200

    Implemented ElGamal and import/export key

--- lib/ElGamal.py
+++ lib/ElGamal.py
@@ -1,8 +1,61 @@
 from zope.interface import implementer
 from .interfaces import IEncryption
-from Crypto.Cipher import DES3
-from Crypto.Util.number import getPrime, long_to_bytes, bytes_to_long, inverse, getRandomRange
 from Crypto.Random.random import randint
+from Crypto.Util.number import getPrime, long_to_bytes, bytes_to_long, inverse, getRandomRange
+from Crypto.Util.py3compat import tobytes, tostr
+from Crypto.Util.asn1 import DerSequence, DerNull
+from Crypto.PublicKey import (_expand_subject_public_key_info,
+                              _create_subject_public_key_info,) # Maybe a problem with this import
+
+
+# TODO: Mozda dodati protekciju za sam PEM format preko nekog passhprase-a, ima u Crypto.PublicKey.RSA ideja
+def _import_private(encoded, *kwargs):
+    # ElGamalPrivateKey ::= SEQUENCE {
+    #           version Version,
+    #           modulus INTEGER, -- p
+    #           publicExponent INTEGER, -- g
+    #           public result of  of g ^ x mod p INTEGER, -- y
+    #           private random INTEGER, -- x
+    # }
+    #
+    # Version ::= INTEGER
+    der = DerSequence().decode(encoded, nr_elements=5, only_ints_expected=True)
+    if der[0] != 0:
+        raise ValueError("No valid encoding of an ElGamal private key")
+    return ElGamalKey.construct(*der[1:])
+
+
+def _import_public(encoded, *kwargs):
+    # ElGamalPublicKey ::= SEQUENCE {
+    #           modulus INTEGER, -- p
+    #           publicExponent INTEGER, -- g
+    #           public result of  of g ^ x mod p INTEGER, -- y
+    # }
+    der = DerSequence().decode(encoded, nr_elements=3, only_ints_expected=True)
+    return ElGamalKey.construct(*der)
+
+def _import_subjectPublicKeyInfo(encoded, *kwargs):
+
+    algoid, encoded_key, params = _expand_subject_public_key_info(encoded)
+    if algoid != ElGamalKey.oid or params is not None:
+        raise ValueError("No ElGamal subjectPublicKeyInfo")
+    return _import_public(encoded_key)
+
+def _import_keyDER(extern_key, passphrase):
+    """Import an ElGamal key (public or private half), encoded in DER form."""
+
+    decodings = (_import_private,
+                 _import_public,
+                 _import_subjectPublicKeyInfo,
+                 )
+
+    for decoding in decodings:
+        try:
+            return decoding(extern_key, passphrase)
+        except ValueError:
+            pass
+
+    raise ValueError("ElGamal key format is not supported")
 
 class ElGamalKey:
     """
@@ -12,6 +65,10 @@ class ElGamalKey:
         - y: part of the public, y = g^x mod p
         - x: private key, random number in between 0 and p-2
     """
+
+    # OID for ElGamal
+    oid = '1.3.14.7.2.1.1'
+
     def __init__(self):
         self.p = None
         self.g = None
@@ -19,12 +76,15 @@ class ElGamalKey:
         self.x = None
         self.has_private_key = False
 
-    def construct(self, p, g, y, x=None):
-        self.p = p
-        self.g = g
-        self.y = y
-        self.x = x
-        self.has_private_key = x is not None
+    @classmethod
+    def construct(cls, p, g, y, x=None):
+        elgamal_key = ElGamalKey()
+        elgamal_key.p = p
+        elgamal_key.g = g
+        elgamal_key.y = y
+        elgamal_key.x = x
+        elgamal_key.has_private_key = x is not None
+        return elgamal_key
         
     def generate_key(self):
         self.p = getPrime(512)
@@ -32,6 +92,9 @@ class ElGamalKey:
         self.g = randint(0, self.p-1)
         self.y = pow(self.g, self.x, self.p)
         self.has_private_key = True
+
+    def has_private(self):
+        return self.has_private_key
         
     def public_key(self):
         return (self.p, self.g, self.y)
@@ -40,6 +103,45 @@ class ElGamalKey:
         if self.has_private_key:
             return (self.p, self.g, self.y, self.x)
         raise ValueError('No private key')
+
+    def export_key(self):
+        """
+        Export ElGamal key into a PEM format.
+        """
+        # DER format is always used, even in case of PEM, which simply
+        # encodes it into BASE64.
+        if self.has_private():
+            binary_key = DerSequence([0,
+                                      self.p,
+                                      self.g,
+                                      self.y,
+                                      self.x,
+                                      ]).encode()
+            
+            key_type = 'ELGAMAL PRIVATE KEY'
+        else:
+            key_type = "PUBLIC KEY"
+            binary_key = _create_subject_public_key_info(ElGamalKey.oid,
+                                                         DerSequence([self.p,
+                                                                      self.g,
+                                                                      self.y,]),
+                                                         DerNull()
+                                                         )
+        from Crypto.IO import PEM
+
+        pem_str = PEM.encode(binary_key, key_type, None, None)
+        return tobytes(pem_str)
+    
+    def import_key(extern_key):
+        """
+        Import an ElGamal key (public or private half), encoded in standard PEM format
+        """
+        from Crypto.IO import PEM
+
+        extern_key = tobytes(extern_key)
+
+        (der, marker, enc_flag) = PEM.decode(tostr(extern_key), None)
+        return _import_keyDER(der, None)
 
     def __str__(self):
         return f'ElGamalKey(p={self.p}, g={self.g}, y={self.y}, x={self.x})'
---
+++ test_elgamal.py
@@ -0,0 +1,46 @@
+from lib.ElGamal import ElGamalKey, ElGamalEncryption
+
+def main():
+    elgamal_key = ElGamalKey()
+    elgamal_key.generate_key()
+    str_key = elgamal_key.__str__()
+    print(str_key)
+    with open('elgamal_key.pem', 'wb') as f:
+        f.write(elgamal_key.export_key())
+    
+    with open('elgamal_key.pem', 'rb') as f:
+        elgamal_key = ElGamalKey.import_key(f.read())
+    
+    str_read_key = elgamal_key.__str__()
+    if str_key == str_read_key:
+        print('OK')
+
+    drugi = ElGamalKey.construct(elgamal_key.p, elgamal_key.g, elgamal_key.y)
+    drugi_str = drugi.__str__()
+    with open('drugi.pem', 'wb') as f:
+        f.write(drugi.export_key())
+
+    with open('drugi.pem', 'rb') as f:
+        drugi = ElGamalKey.import_key(f.read())
+
+    drugi_str_read = drugi.__str__()
+    if drugi_str == drugi_str_read:
+        print('OK')
+
+
+    
+    # elgamal_enc = ElGamalEncryption(elgamal_key)
+    # m = 'cao'
+    # m  = m.encode()
+    # print('m_b', m)
+    # ciphertext = elgamal_enc.encrypt(m)
+    # print(ciphertext)
+
+    # print()
+
+    # plaintext = elgamal_enc.decrypt(ciphertext)
+    # print(plaintext)
+
+
+if __name__ == '__main__':
+    main()
